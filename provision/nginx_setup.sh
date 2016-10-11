e3cbcf619774f17c130719dada050f76e4dfc1e7
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-10-11 00:54:09 -0700

    Changed the comments in nginx_setup.sh

--- provision/nginx_setup.sh
+++ provision/nginx_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+
+# Create alternative html and logs directory for future use
 mkdir -p /var/www/example.com/html/ /var/www/example.com/logs/
 
-echo $USER
-echo "THE USER IS $USER!"
+# Some nginx sets the default user as nobody, use if necessary
 adduser nobody
 sudo chown -R nobody:nobody /var/www/example.com/html
 sudo chmod 755 /var/www
 
-echo "THE USER IS STILL $USER"
-
+echo "Adding docker and www groups to the user"
 sudo groupadd docker # > /dev/null 2>&1
 sudo usermod -aG docker $USER # > /dev/null 2>&1
 sudo groupadd www # > /dev/null 2>&1
